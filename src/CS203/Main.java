77d742ac4742c2dc7895b8ad3b61a53374021e99
OriginalMidas <clayton.brez@gmail.com>
2019-03-02 15:30:21 -0600

    add user data collection to project

--- src/CS203/Main.java
+++ src/CS203/Main.java
@@ -6,10 +6,11 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        double[][] inputMatrix = new double[][]{
-                {0.15, 0.875, 0.375},
-                {0.55, 0.005, 0.225},
-                {0.30, 0.12, 0.4}};
+//        double[][] inputMatrix = new double[][]{
+//                {0.15, 0.875, 0.375},
+//                {0.55, 0.005, 0.225},
+//                {0.30, 0.12, 0.4}};
+        double[][] inputMatrix = getUserInfo();
         double[][] sortedMatrix = sortColumns(inputMatrix);
         print(sortedMatrix);
     }
@@ -25,12 +26,36 @@ public class Main {
         }
         return m;
     }
-    public static void print(double[][] inputArray){
-        for(double[] arrayRow: inputArray){
-            for (double element: arrayRow){
+
+    public static void print(double[][] inputArray) {
+        for (double[] arrayRow : inputArray) {
+            for (double element : arrayRow) {
                 System.out.print(element + " ");
             }
             System.out.println();
         }
+    }
+
+    public static double[][] getUserInfo() {
+        double[][] twoDArray = new double[3][3];
+
+        System.out.println("Please enter a 3x3 array, row by row, with a space in between each number. And Enter between rows");
+        // Reading from System.in
+        Scanner input = new Scanner(System.in);
+
+        // loop through the array,
+        for (double[] row : twoDArray) {
+            String inputString = input.nextLine();
+            String[] individualNumbers = inputString.split(" ");
+            for (int i = 0; i < row.length; i++) {
+                try {
+                    row[i] = Double.parseDouble(individualNumbers[i]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Due to an incorrect input, we are setting that incorrect value to 1.0");
+                    row[i] = 1.0;
+                }
+            }
+        }
+        return twoDArray;
     }
 }
