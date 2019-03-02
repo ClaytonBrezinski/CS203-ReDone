2f5d9c23aef037d18740285fb2dd18b39c20583a
OriginalMidas <clayton.brez@gmail.com>
2019-03-02 11:31:19 -0600

    add test matrix to project

--- src/CS203/Main.java
+++ src/CS203/Main.java
@@ -1,8 +1,18 @@
 package CS203;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double[][] InputMatrix = {
+                {0.15, 0.875, 0.375},
+                {0.55, 0.005, 0.225},
+                {0.30, 0.12, 0.4}};
+        double[][] sortedMatrix = sortColumns(inputMatrix);
+    }
+    public static double[][] sortColumns(double[][] m) {
+        return m;
     }
 }
