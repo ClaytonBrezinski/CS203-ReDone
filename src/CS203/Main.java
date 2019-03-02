48ee83ce5c37ab3638d9739cec806942f73ee971
OriginalMidas <clayton.brez@gmail.com>
2019-03-02 11:52:45 -0600

    add array sort logic and print

--- src/CS203/Main.java
+++ src/CS203/Main.java
@@ -6,13 +6,31 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        double[][] InputMatrix = {
+        double[][] inputMatrix = new double[][]{
                 {0.15, 0.875, 0.375},
                 {0.55, 0.005, 0.225},
                 {0.30, 0.12, 0.4}};
         double[][] sortedMatrix = sortColumns(inputMatrix);
+        print(sortedMatrix);
     }
+
     public static double[][] sortColumns(double[][] m) {
+//        for (int i = 0; i < m.length; i++) {
+//            // passes by reference
+//            Arrays.sort(m[i]);
+//        }
+        for (double[] arrayRow : m) {
+            // passes by reference
+            Arrays.sort(arrayRow);
+        }
         return m;
+    }
+    public static void print(double[][] inputArray){
+        for(double[] arrayRow: inputArray){
+            for (double element: arrayRow){
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
     }
 }
